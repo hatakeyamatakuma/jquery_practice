9a3f7bcf36fbef29ff26fc4868484c3ea9fa347b
hatakeyamatakuma <hatakuma@icloud.com>
2022-06-06 20:05:57 +0900

    10commit q8 on the way

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -1,68 +1,105 @@
+//ページの読み込みが終わったら処理を実行する
 $(function() {
+  //pageCountの初期値は１
+  let pageCount = 1;
+  //ひとつ前の検索履歴の保管用
+  let searchResult = "";
   //.search-btnをクリックすると処理を開始する
   $(".search-btn").on("click",function(){
     //入力した文字をsearchWordに置き換える
-    const searchWord = $("#search-input").val();
-    console.log(searchWord);
-    //pageCountは初期値が１。同じ検索ワードなら＋１。違う場合は１に戻す。
-    const pageCount = 1;
+    let searchWord = $("#search-input").val();
+    //同じ検索ワードなら＋１。違う場合は１に戻す
+    if(searchWord === searchResult){
+      pageCount = ++pageCount;
+    }else{
+      pageCount = 1
+    };
+    ///検索ワードを次回検索で比較するために保管する
+    searchResult = searchWord;
     const settings = {
         //titleは部分一致検索、formatはフォーマット指定。ここではjson。pはページ番号。countは１ページあたりの表示数。
         "url": `https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&format=json&p=${pageCount}&count=20`,
         "method": "GET",
     };
     $.ajax(settings)
-    　.done(function (response) {
-      console.log("AJAX通信は成功")//ここの処理が実行された時点でajax通信自体は成功している
+    //検索した情報がここに入る
+    .done(function (response) {
+      console.log("AJAX通信は成功")
+      //取得した情報をresultという変数に変換
       const result = response['@graph'];
-      displayResult(result)//ここで関数を呼び出して要素を表示する処理を行う
+      //関数を呼び出して要素を表示する処理を行う
+      displayResult(result)
     })
-    　.fail(function (err) {
-      console.log("AJAX通信失敗！");
+    //AJAXのやりとりでエラーが発生した場合以下の処理が実行される
+    .fail(function (err) {
+      console.log("AJAX通信失敗");
+      //AJAX通信が失敗したかの確認
       displayError(err)
     });
   })
 });
 
 
-
-
-function displayResult(fn){//一度に２０個表示
-  const content = fn["0"]["items"]["0"]
-  console.log(content);
-  const title = content["title"];
-  const author = content["dc:creator"];
-  const publisher = content["dc:publisher"]["0"];
-  const link = content["link"]["@id"];
-
-  
-  //fn 0 items 要素２０　の指定を省略する方法を探す。
-
-
-  $("ul").append("<li>");
-  $(".lists li").addClass("lists-item");
-  $(".lists-item").append("<div>");
-  $(".lists-item div").addClass("lists-inner");
-  $(".lists-inner").append("<p>");
-  $(".lists-inner p").eq(0).text("タイトル:");
-  $(".lists-inner p").eq(0).append(title);
-  $(".lists-inner").append("<p>");
-  $(".lists-inner p").eq(1).text("作者:");
-  $(".lists-inner p").eq(1).append(author);
-  $(".lists-inner p").eq(1).append()
-  $(".lists-inner").append("<p>");
-  $(".lists-inner p").eq(2).text("出版社:");
-  $(".lists-inner p").eq(2).append(publisher);
-  $(".lists-inner").append("<a>");
-  $(".lists-inner a").text("書籍情報");
-  $(".lists-inner a").attr("href",link);
+//呼び出される関数
+function displayResult(fn){
+  //検索結果の操作したい部分に合わせている
+  const content = fn["0"]["items"];
+  //20回の繰り返し処理
+  $(content).each(function(index, title){
+    //検索結果の中のタイトルや作者名等を変数として定義
+    const contentTitle = content[index]["title"];
+    const author = content[index]["dc:creator"];
+    const publisher = content[index]["dc:publisher"][0]
+    const link = content[index]["link"]["@id"];
+    //liを作成
+    $("ul").prepend("<li>");
+    //liにクラス名をつける
+    $(".lists li").addClass("lists-item");
+    //一番上のlists-itemの中にdivを作成
+    $(".lists-item").eq(0).append("<div>");
+    //divにクラス名をつける
+    $(".lists-item div").addClass("list-inner");
+    //一番上のlist-innerにp３つa1つ追加する
+    $(".list-inner").eq(0).append("<p>","<p>","<p>","<a>");
+    //list-inner pのそれぞれに固定の文字列、適切なデータを配置
+    $(".list-inner p:nth-child(1)").eq(0).append("タイトル：",contentTitle);
+    $(".list-inner p:nth-child(2)").eq(0).append("作者：",author);
+    $(".list-inner p:nth-child(3)").eq(0).append("出版社：",publisher);
+    //lists-inner aに固定の文字列、適切なリンクを配置
+    $(".list-inner a").eq(0).append("書籍情報").attr("href",link);
+    });
 }
+/*
+$(function(){
+  $("ul").append("<li>");
+    $(".lists li").addClass("lists-item");///////////////////////////ここまでは動作問題なし！！！！！！！！！
+    $(".lists-item").eq(index).append("<div>");
+    $(".lists-item div").addClass("list-inner");//固定！！！！
+    $(".list-inner").eq(index).append("<p>","<p>","<p>","<a>");//html要素を必要な数展開。
+    $(".list-inner p:nth-child(1)").eq(index).append("タイトル:",contentTitle);//pのそれぞれに固定の文字列を追加する。
+    $(".list-inner p:nth-child(2)").eq(index).append("作者:",author);
+    $(".list-inner p:nth-child(3)").eq(index).append("出版社:",publisher);
+    $(".list-inner a").eq(index).append("書籍情報").attr("href",link);
+})*/
 
 
+    /*
+    console.log(pageCount);
+    console.log(searchWord);
+    $(".search-btn").on(click,function(){
+      let test = $("#search-input").val();
+      console.log(test);
+    })
+        $("#search-input").change(function() {
+      let val1 = $(this).val();// valueを取得
+      console.log(val1);
+    });*/
+//セレクタがオブジェクトだからattrはエラーになる？セレクタがオブジェクトだとエラーになるメソッドも存在する。appendは平気なやつ？
+//$(".lists-inner a")ここは問題ない、他のコードを当てはめたら動く
+//("href",link)引数も問題ない
+//問題があるのは[].attr()の部分。attrの前に数字がつくとなぜかエラー。.appendは動く。エラーの原因を調べる必要有り。
 
 
-
-//lists ulの子要素にlist-itemを追加する。その内容は検索結果の内容が入る
 
 
 /*　　『動きの言語化』
