0d41265f301f2ea68732feef6e3dda4af08efc7c
hatakeyamatakuma <hatakuma@icloud.com>
2022-06-12 22:25:01 +0900

    13commit q5revice

--- q5/work/js/app.js
+++ q5/work/js/app.js
@@ -2,10 +2,10 @@ $(".dropdwn li").on({
   //dropdwn liにマウスを置いた時処理を行う
   "mouseenter": function(){
   //隠していた要素を表示する
-    $("ul:not(:animated)",this).slideDown();
+    $(this).children("ul").stop().slideDown();
   //dropdwn liからマウスが離れた時に処理を行う
 },"mouseleave": function(){
   //表示していた要素を非表示する処理を行う
-  $("ul:not(:animated)",this).slideUp();
+  $(this).children("ul").stop().slideUp();
 }
 });
