cfc57de2f0ca49d2a8a5c08882d73074426e0a82
hatakeyamatakuma <hatakuma@icloud.com>
2022-06-13 18:04:06 +0900

    14commit q6revice

--- q6/work/js/app.js
+++ q6/work/js/app.js
@@ -2,15 +2,25 @@ $(function () {
     //select-boxで表示されているデータが違うものになった時処理を行う
   $(".select-box").on("change", function () {
     //セレクトボックスで選択された部分のvalueをselectValとして設定
-    const selectVal = $(this).val(),
+    const selectVal = $(this).val();
     //food-listのliをfoodLiと設定
-    foodLi = $(".food-list li");
+    const foodList = $(".food-list li");
     //三項演算子、value値がallだった場合foodlistを全て表示する
-    "all" === selectVal ? foodLi.show() :
-    //allでなかった場合、eachを使って繰り返し処理、foodLiのvalueをfoodValとする。
-    $.each(foodLi, function (index, foodVal) {
-    //この二行は繰り返しの処理。foodValのcategory-typeの中身を取得、categoryTypeとする
-    const categoryType = $(foodVal).data("category-type");
-    //三項演算子、selectValとcategoryTypeが同一であればそのfoodValの表示、違えばfoodValを非表示
-    selectVal === categoryType ? $(foodVal).show() : $(foodVal).hide()
-    }) }) });
+    if(selectVal === "all"){
+      foodList.show();
+    }else{
+      //繰り返しの処理
+      $.each(foodList, function (index, foodVal) {
+        //foodValのcategory-typeの中身を取得、categoryTypeとする
+        const categoryType = $(foodVal).data("category-type");
+        //selectValとcategoryTypeが同一であればそのfoodValの表示、違えばfoodValを非表示
+        if(selectVal === categoryType){
+          $(foodVal).show();
+        }else{
+          $(foodVal).hide()
+        };
+      });
+    };
+  });
+});
+
